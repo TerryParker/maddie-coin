9b0a24b935bd93b4414e998f3871ca78ed1860a5
Terry Parker <tparker@watchsystems.com>
2021-06-11 01:22:28 -0500

    updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,25 +16,53 @@ export default function Home() {
   //let firstTransaction = new Transaction('Olivia', 'Maddie', 10);
   //firstTransaction.signTransaction(newKeys);
   //blockChainInstance.addTransaction(firstTransaction);
-  //blockChainInstance.minePendingTransactions('Olivia');
+  blockChainInstance.minePendingTransactions('Olivia');
   
   return (
     
     <>
      <h1>Blocks on the Blockchain</h1>
      <div>
-     <Card style={{ width: '18rem' }}>
+     <div style={{padding: '25px 50px 75px 100px'}}>
+     <Card style={{ width: '18rem' , backgroundColor: 'lightgrey'}}>
           <Card.Body>
             <Card.Title>{blockChainInstance.chain[0].transactions}</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">Genesis Block</Card.Subtitle>
                 <Card.Text>
+                  <div>Block Hash:</div>
                   {blockChainInstance.chain[0].hash}
                 </Card.Text>
                 <Card.Text>
+                  <div>Previous Block hash:</div>
+                  {blockChainInstance.chain[0].previousHash}
+                </Card.Text>
+                <Card.Text>
+                  <div>Balance:</div>
                   {blockChainInstance.getBalanceOfAddress(key.getPublic('hex'))}
                 </Card.Text>
           </Card.Body>
       </Card>
+      </div>
+      <div style={{padding: '0px 50px 75px 100px'}}>
+      <Card style={{ width: '18rem' , backgroundColor: 'lightgrey'}}>
+          <Card.Body>
+            <Card.Title>{blockChainInstance.chain[1].nonce}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Next Block</Card.Subtitle>
+                <Card.Text>
+                  <div>Block Hash:</div>
+                  {blockChainInstance.chain[1].hash}
+                </Card.Text>
+                <Card.Text>
+                  <div>Previous Block hash:</div>
+                  {blockChainInstance.chain[1].previousHash}
+                </Card.Text>
+                <Card.Text>
+                  <div>Balance:</div>
+                  {blockChainInstance.getBalanceOfAddress(key.getPublic('hex'))}
+                </Card.Text>
+          </Card.Body>
+      </Card>
+      </div>
       </div>
     </>
 
